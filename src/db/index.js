29dad4cdcc56801d53f6aef6ba215fad2992b90c
Vasu Sharma <vsh.9705@gmail.com>
2025-08-07 23:44:25 +0530

    changes to db file

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     try {
         const connect = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected - DB HOST: ${connect.connection.host}`);
-        
     } catch (error) {
         console.log("MONGODB connection FAILED: ", error);
         process.exit(1)
