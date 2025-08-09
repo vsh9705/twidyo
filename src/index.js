b11a3d067a269650e0ef2ff799278fffb6d73297
Vasu <vsh.9705@gmail.com>
2025-08-09 20:25:31 +0000

    middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.error("Database connection error:", error);
+    process.exit(1);
+});
 
 
 // import express from "express"
