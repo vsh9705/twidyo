eb0a4a558310ae04f7f10a6419f4d965f2d8f6fe
Vasu <vsh.9705@gmail.com>
2025-08-10 20:32:20 +0000

    configured cloudinary for file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) {
+            return null;
+        }
+        // upload the file to cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto',
+        })
+        // file has been uploaded
+        console.log("File uploaded successfully to Cloudinary");
+        console.log(response.url);
+        return response;
+    }
+    catch (error) {
+        fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation failed
+        return null;
+    }
+}
+
+export { uploadOnCloudinary }
+
+// const uploadResult = await cloudinary.uploader
+//     .upload(
+//         'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//             public_id: 'shoes',
+//         }
+//     )
+//     .catch((error) => {
+//         console.log(error);
+//     });
+    
+// console.log(uploadResult);
