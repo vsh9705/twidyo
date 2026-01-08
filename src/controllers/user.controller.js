bd50f0b1e4a13e40193e975379ddb981146fa93e
Vasu <vsh.9705@gmail.com>
2026-01-08 14:46:19 +0000

    sub pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -359,4 +359,54 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
     .json(new ApiResponse(200, channel[0], "User channel fetched succesfully"))
 })
 
-export {getUserChannelProfile, updateUserCoverImage, updateUserAvatar, updateAccountDetails, registerUser, loginUser, logoutUser, refreshAccessToken, getCurrentUser, changeCurrentPassword };
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user[0].watchHistory, "Watch History Fetched successfully!")
+    )
+})
+
+export {getWatchHistory, getUserChannelProfile, updateUserCoverImage, updateUserAvatar, updateAccountDetails, registerUser, loginUser, logoutUser, refreshAccessToken, getCurrentUser, changeCurrentPassword };
